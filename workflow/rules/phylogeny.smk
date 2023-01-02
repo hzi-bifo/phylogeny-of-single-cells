a7d82826b4117552217290a0a86b5316283666b9
David Laehnemann <david.laehnemann@helmholtz-hzi.de>
2023-01-02 14:21:02 +0100

    initial rules to prepare prosolo calls for raxml-ng

---
+++ workflow/rules/phylogeny.smk
@@ -0,0 +1,34 @@
+rule prosolo_probs_to_raxml_ng_genotypes_per_cell:
+    input:
+        "results/genotype_fdr/{individual}/{sc}.merged_bulk.prosolo.sorted.{genotype}.fdr_controlled.bcf",
+    output:
+        "results/raxml_ng/{individual}/per_genotype/{sc}.{genotype}.genotype_likelihoods.tsv"
+    log:
+        "logs/raxml_ng/{individual}/per_genotype/{sc}.{genotype}.genotype_likelihoods.log"
+    conda:
+        "../envs/vembrane_vlr_bcftools.yaml"
+    shell:
+        "( varlociraptor decode-phred < {input} |"
+        "  vembrane table "
+        "    --header 'CHROM, POS, REF, ALT, HOM_REF, HET, HOM_ALT' "
+        "    'CHROM, POS, REF, ALT, INFO[\"PROB_HOM_REF\"] + INFO[\"PROB_ERR_ALT\"], "
+        "     INFO[\"PROB_ADO_TO_ALT\"] + INFO[\"PROB_HET\"] + INFO[\"PROB_ADO_TO_REF\"], "
+        "     INFO[\"PROB_HOM_ALT\"] + INFO[\"PROB_ERR_REF\"]' "
+        "  >{output}"
+        ") 2> {log}"
+
+
+rule merge_raxml_ng_genotypes_per_cell:
+    input:
+        hom_ref="results/raxml-ng/{individual}/per_genotype/{sc}.hom_ref.genotype_likelihoods.tsv",
+        het="results/raxml-ng/{individual}/per_genotype/{sc}.het.genotype_likelihoods.tsv",
+        hom_alt="results/raxml-ng/{individual}/per_genotype/{sc}.hom_alt.genotype_likelihoods.tsv",
+    output:
+        "results/raxml-ng/{individual}/{sc}.genotype_likelihoods.tsv"
+    log:
+        "logs/raxml-ng/{individual}/{sc}.genotype_likelihoods.tsv"
+    conda:
+        "../envs/tidyverse.yaml"
+    script:
+        "../scripts/merge_raxml_ng_genotypes_per_cell.R"
+
---
+++ workflow/scripts/merge_raxml_ng_genotypes_per_cell.R
@@ -0,0 +1,80 @@
+log <- file(snakemake@log[[1]], open="wt")
+sink(log)
+sink(log, type="message")
+
+raxml_ng_genotype_lookup_table <- tribble(
+   ~a,  ~b, ~iupac,
+  "A", "A",    "A",
+  "C", "C",    "C",
+  "G", "G",    "G",
+  "T", "T",    "T",
+  "A", "C",    "M",
+  "A", "G",    "R",
+  "A", "T",    "W",
+  "C", "A",    "M",
+  "C", "G",    "S",
+  "C", "T",    "Y",
+  "G", "A",    "R",
+  "G", "C",    "S",
+  "G", "T",    "K",
+  "T", "A",    "W",
+  "T", "C",    "Y",
+  "T", "G",    "K"
+)
+
+iupac_of <- function(a, b, table) {
+  return table %>%
+    filter( a == a, b == b) %>%
+    pull(iupac)
+}
+
+to_raxml_ng_genotype <- function(genotype, ref, alt, lookup_table) {
+  if ( genotype == "hom_ref" ) {
+    iupac <- iupac_of(ref, ref, lookup_table)
+  } else if ( genotype == "het" ) {
+    iupac <- iupac_of(ref, alt, lookup_table)
+  } else if ( genotype == "hom_alt" ) {
+    iupac <- iupac_of(alt, alt, lookup_table)
+  } else {
+    cli_abort("Unknown genotype name. Please only use 'hom_ref', 'het', or 'hom_alt'.")
+  }
+  iupac
+}
+
+raxml_ng_genotype_order <- c("A", "C", "G", "T", "M", "R", "W", "S", "Y", "K")
+
+to_raxml_ng_genotype_likelihoods <- function(ref, alt, lookup_table, order, hom_ref_likelihood, het_likelihood, hom_alt_likelihood) {
+  likelihoods = rep(0.0, 10)
+
+  hom_ref_index <- which(iupac_of(ref, ref, lookup_table), order)
+  likelihoods[hom_ref_index] <- hom_ref_likelihood
+
+  het_index <- which(iupac_of(ref, alt, lookup_table), order)
+  likelihoods[het_index] <- het_likelihood
+
+  hom_alt_index <- which(iupac_of(alt, alt, lookup_table), order)
+  likelihoods[hom_alt_index] <- hom_alt_likelihood
+
+  likelihoods
+}
+
+read_genotype <- function(genotype) {
+  return read_tsv(snakemake@input[[genotype]]) %>%
+    mutate(
+      raxml_ng_gt = to_raxml_ng_genotype(genotype, REF, ALT, raxml_ng_genotype_lookup_table)
+      raxml_ng_gt_likelihoods = to_raxml_ng_genotype_likelihoods(REF, ALT, raxml_ng_genotype_lookup_table, HOM_REF, HET, HOM_ALT)
+    ) %>%
+    select(
+      !c(HOM_REF, HET, HOM_ALT)
+    )
+}
+
+all_genotypes <- read_genotype("hom_ref") %>%
+  bind_rows(
+    read_genotype("het")
+  ) %>%
+  bind_rows(
+    read_genotype("hom_alt")
+  )
+
+write_tsv(all_genotypes, snakemake@output[[1]])
