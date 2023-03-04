e1c07c24ed09d2f97689a23b088f380311803f92
David Laehnemann <david.laehnemann@helmholtz-hzi.de>
2023-03-04 12:58:00 +0100

    fix merge_raxml_ng_genotypes_per_cell.R script code

--- workflow/scripts/merge_raxml_ng_genotypes_per_cell.R
+++ workflow/scripts/merge_raxml_ng_genotypes_per_cell.R
@@ -4,39 +4,32 @@ sink(log, type="message")
 
 library(tidyverse)
 
-raxml_ng_genotype_lookup_table <- tribble(
-   ~r,  ~a, ~iupac,
-  "A", "A",    "A",
-  "C", "C",    "C",
-  "G", "G",    "G",
-  "T", "T",    "T",
-  "A", "C",    "M",
-  "A", "G",    "R",
-  "A", "T",    "W",
-  "C", "A",    "M",
-  "C", "G",    "S",
-  "C", "T",    "Y",
-  "G", "A",    "R",
-  "G", "C",    "S",
-  "G", "T",    "K",
-  "T", "A",    "W",
-  "T", "C",    "Y",
-  "T", "G",    "K"
+lookup_table <- c(
+  "AA" = "A",
+  "CC" = "C",
+  "GG" = "G",
+  "TT" = "T",
+  "AC" = "M",
+  "AG" = "R",
+  "AT" = "W",
+  "CA" = "M",
+  "CG" = "S",
+  "CT" = "Y",
+  "GA" = "R",
+  "GC" = "S",
+  "GT" = "K",
+  "TA" = "W",
+  "TC" = "Y",
+  "TG" = "K"
 )
 
-iupac_of <- function(ref, alt, table) {
-  table %>%
-    filter( r == ref, a == alt) %>%
-    pull(iupac)
-}
-
-to_raxml_ng_genotype <- function(genotype, ref, alt, lookup_table) {
-  if ( genotype == "hom_ref" ) {
-    iupac <- iupac_of(ref, ref, lookup_table)
-  } else if ( genotype == "het" ) {
-    iupac <- iupac_of(ref, alt, lookup_table)
-  } else if ( genotype == "hom_alt" ) {
-    iupac <- iupac_of(alt, alt, lookup_table)
+to_raxml_ng_genotype <- function(ref, alt, gt, iupac_of) {
+  if ( gt == "hom_ref" ) {
+    iupac <- iupac_of[[str_c(ref, ref)]]
+  } else if ( gt == "het" ) {
+    iupac <- iupac_of[[str_c(ref, alt)]]
+  } else if ( gt == "hom_alt" ) {
+    iupac <- iupac_of[[str_c(alt, alt)]]
   } else {
     cli_abort("Unknown genotype name. Please only use 'hom_ref', 'het', or 'hom_alt'.")
   }
@@ -45,16 +38,16 @@ to_raxml_ng_genotype <- function(genotype, ref, alt, lookup_table) {
 
 raxml_ng_genotype_order <- c("A", "C", "G", "T", "M", "R", "W", "S", "Y", "K")
 
-to_raxml_ng_genotype_likelihoods <- function(ref, alt, lookup_table, order, hom_ref_likelihood, het_likelihood, hom_alt_likelihood) {
+to_raxml_ng_genotype_likelihoods <- function(ref, alt, hom_ref_likelihood, het_likelihood, hom_alt_likelihood, iupac_of, order) {
   likelihoods = rep(0.0, 10)
 
-  hom_ref_index <- which(iupac_of(ref, ref, lookup_table), order)
+  hom_ref_index <- which(iupac_of[[str_c(ref, ref)]] == order)
   likelihoods[hom_ref_index] <- hom_ref_likelihood
 
-  het_index <- which(iupac_of(ref, alt, lookup_table), order)
+  het_index <- which(iupac_of[[str_c(ref, alt)]] == order)
   likelihoods[het_index] <- het_likelihood
 
-  hom_alt_index <- which(iupac_of(alt, alt, lookup_table), order)
+  hom_alt_index <- which(iupac_of[[str_c(alt, alt)]] == order)
   likelihoods[hom_alt_index] <- hom_alt_likelihood
 
   str_c(likelihoods, collapse=",")
@@ -63,8 +56,29 @@ to_raxml_ng_genotype_likelihoods <- function(ref, alt, lookup_table, order, hom_
 read_genotype <- function(genotype) {
   read_tsv(snakemake@input[[genotype]]) %>%
     mutate(
-      ml_genotype = to_raxml_ng_genotype(genotype, REF, ALT, raxml_ng_genotype_lookup_table),
-      likelihoods = to_raxml_ng_genotype_likelihoods(REF, ALT, raxml_ng_genotype_lookup_table, HOM_REF, HET, HOM_ALT)
+      ml_genotype = pmap_chr(
+        .,
+        function(REF, ALT, ... )
+        to_raxml_ng_genotype(
+          ref= REF,
+          alt = ALT,
+          gt = genotype,
+          iupac_of = lookup_table
+        )
+      ),
+      likelihoods = pmap_chr(
+        .,
+        function(REF, ALT, HOM_REF, HET, HOM_ALT, ...)
+        to_raxml_ng_genotype_likelihoods(
+          ref = REF,
+          alt = ALT, 
+          hom_ref_likelihood = HOM_REF,
+          het_likelihood = HET,
+          hom_alt_likelihood = HOM_ALT,
+          iupac_of = lookup_table,
+          order = raxml_ng_genotype_order
+        )
+      )
     ) %>%
     select(
       !c(HOM_REF, HET, HOM_ALT)
@@ -78,6 +92,6 @@ all_genotypes <- read_genotype("hom_ref") %>%
   bind_rows(
     read_genotype("hom_alt")
   ) %>%
-  add_column(cell = snakemake@wildcards.sc)
+  add_column(cell = snakemake@wildcards[["sc"]])
 
 write_tsv(all_genotypes, snakemake@output[[1]])
