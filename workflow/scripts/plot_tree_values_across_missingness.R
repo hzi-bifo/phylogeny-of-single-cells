7ebc0bb3b0149959fce8a7f89fbf6ecaa7b8c0c6
dlaehnemann <david.laehnemann@uk-essen.de>
2025-03-24 21:05:52 +0000

    fix: better error backtrace

--- workflow/scripts/plot_tree_values_across_missingness.R
+++ workflow/scripts/plot_tree_values_across_missingness.R
@@ -2,9 +2,10 @@ log <- file(snakemake@log[[1]], open="wt")
 sink(log)
 sink(log, type="message")
 
-
 library("treeio")
 library("tidyverse")
+
+rlang::global_entrace()
 
 best_trees_support_values <- snakemake@input[["support_trees"]] |>
   map(
